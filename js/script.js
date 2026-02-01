ccb8fd1f127fb2b853890d14d62013cce5d44c54
prince-mutale-71 <prince7mutale1@gmail.com>
2026-02-01 15:17:48 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Hamburger toggle
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+hamburger.addEventListener('click', ()=>{
+    navLinks.classList.toggle('active');
+});
